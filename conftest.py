76650d84e40646a23a172a14e138dfa011c9c450
GrzegorzL <grzegorzl@MacBook-Pro.local>
2026-06-08 15:30:32 +0200

    conftest - browser_type_launch_args

--- conftest.py
+++ conftest.py
@@ -41,6 +41,14 @@ def browser_context_args(browser_context_args):
         "record_video_dir": None,
     }
 
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args):
+    return {
+        **browser_type_launch_args,
+        "headless": True,
+    }
+
+
 
 # ── Helpers ───────────────────────────────────────────────────────────────────
 
